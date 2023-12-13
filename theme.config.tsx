1bdaa36e1529fd075ae7aae54ca0e02c6efa70c3
Nichola Saputra <72346253+dev-nichola@users.noreply.github.com>
2023-12-13 13:57:17 +0700

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: {
-  logo: <span><b>Wisata Blitar</b></span>,
+  logo: <span><strong>Wisata Blitar</strong></span>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
